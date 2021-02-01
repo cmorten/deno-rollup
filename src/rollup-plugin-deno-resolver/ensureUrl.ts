89b7cdb040f956115ee77ba08ae069b70956a5a6
cmorten <craig.morten@hotmail.co.uk>
2021-02-01 09:27:08 +0000

    fix: formatting

--- src/rollup-plugin-deno-resolver/ensureUrl.ts
+++ src/rollup-plugin-deno-resolver/ensureUrl.ts
@@ -14,13 +14,25 @@ export function ensureUrl(source: string): string | null {
   if (RE_URL.test(source)) {
     return source;
   } else if (RE_PATH_MALFORMED_HTTP_URL.test(source)) {
-    console.log("http", source, source.replace(RE_PATH_MALFORMED_HTTP_URL, "$1/$3"));
+    console.log(
+      "http",
+      source,
+      source.replace(RE_PATH_MALFORMED_HTTP_URL, "$1/$3"),
+    );
     return source.replace(RE_PATH_MALFORMED_HTTP_URL, "$1/$3");
   } else if (RE_WIN_PATH_MALFORMED_FILE_URL.test(source)) {
-    console.log("win file", source, source.replace(RE_WIN_PATH_MALFORMED_FILE_URL, "$1/$3"));
+    console.log(
+      "win file",
+      source,
+      source.replace(RE_WIN_PATH_MALFORMED_FILE_URL, "$1/$3"),
+    );
     return source.replace(RE_WIN_PATH_MALFORMED_FILE_URL, "$1/$3");
   } else if (RE_PATH_MALFORMED_FILE_URL.test(source)) {
-    console.log("file", source, source.replace(RE_PATH_MALFORMED_FILE_URL, "$1//$3"));
+    console.log(
+      "file",
+      source,
+      source.replace(RE_PATH_MALFORMED_FILE_URL, "$1//$3"),
+    );
     return source.replace(RE_PATH_MALFORMED_FILE_URL, "$1//$3");
   }
 
