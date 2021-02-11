bd1811179ae1c9e40ebf260424bf7c5b2d788cd5
Erick Sosa <ericksosagarcias@gmail.com>
2021-02-11 15:02:36 -0400

    update: use versioned url

--- plugins/svelte/deps.ts
+++ plugins/svelte/deps.ts
@@ -3,9 +3,9 @@ export * as path from "https://deno.land/std@0.86.0/path/mod.ts";
 export {
   compile,
   preprocess,
-} from "https://raw.githubusercontent.com/crewdevio/Snel/main/compiler/core.js";
+} from "https://deno.land/x/snel@v0.0.5/compiler/core.js";
 
 export type {
   compileOptions,
   PreprocessorGroup,
-} from "https://raw.githubusercontent.com/crewdevio/Snel/main/compiler/types.ts";
+} from "https://deno.land/x/snel@v0.0.5/compiler/types.ts";
