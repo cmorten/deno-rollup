69de7ccc17e052905cca45214c0b0b51946bb6d4
denyncrawford <kiquerangelb@gmail.com>
2021-04-20 13:06:04 -0300

    Adding examples

---
+++ examples/serve/rollup.config.ts
@@ -0,0 +1,16 @@
+import serve from 'https://deno.land/x/drollup_plugin_serve@1.1.0+0.1.3/mod.ts'
+
+export default {
+  input: 'src/entry.ts',
+  output: {
+    file: 'src/dest.js',
+    format: 'cjs',
+  },
+  plugins: [
+    serve({ 
+      contentBase: 'src', 
+      port: Math.round(Math.random() * 10000) + 40000,
+      historyApiFallback: '/index.html'
+    }),
+  ],
+}
---
+++ examples/serve/src/entry.ts
@@ -0,0 +1,2 @@
+document.querySelector("#content").innerHTML = `<h1>This is a history fallback enabled website.</h1>
+<p>Reach any endpoint and you will see me.</p>`
