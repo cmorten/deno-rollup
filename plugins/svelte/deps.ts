572ab636eaaa8e5023345f79db4c7efa38299d1f
lfmaciasr <luisf.macias.ramos@gmail.com>
2021-05-31 18:52:01 +0200

    [NO-ISSUE] Fixes some bugs regarding css and svelte plugins (#36)
    * Update mod.ts
    
    Fix bug regarding the output option being typeof string
    
    * Update deps.ts
    
    update dependencies to use an almost up-to-date version of snel compiler: 0.0.5 to 0.5.0
    
    * Update loader.js
    
    fix call to compile method to support options defined by user config since they were always overriden.

--- plugins/svelte/deps.ts
+++ plugins/svelte/deps.ts
@@ -3,9 +3,9 @@ export * as path from "https://deno.land/std@0.91.0/path/mod.ts";
 export {
   compile,
   preprocess,
-} from "https://deno.land/x/snel@v0.0.5/compiler/core.js";
+} from "https://deno.land/x/snel@v0.5.0/compiler/compiler.ts";
 
 export type {
   compileOptions,
   PreprocessorGroup,
-} from "https://deno.land/x/snel@v0.0.5/compiler/types.ts";
+} from "https://deno.land/x/snel@v0.5.0/compiler/types.ts";
