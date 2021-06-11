3c227b7828e398699b8b5fffe530e829b6ea157e
lfmaciasr <luisf.macias.ramos@gmail.com>
2021-06-12 00:05:00 +0200

    [FEAT]: Adds a new rollup plugin called string derived from https://github.com/TrySound/rollup-plugin-string (#37)
    * Update mod.ts
    
    Fix bug regarding the output option being typeof string
    
    * Update deps.ts
    
    update dependencies to use an almost up-to-date version of snel compiler: 0.0.5 to 0.5.0
    
    * Update loader.js
    
    fix call to compile method to support options defined by user config since they were always overriden.
    
    * Create mod.ts
    
    * Create LICENSE.md
    
    * Add derived contribution and original author info
    
    * Create README.md
    
    * Add entry for string plugin
    
    * Update plugins/string/mod.ts
    
    Nice one cmorten!
    
    Co-authored-by: Craig Morten <cmorten@users.noreply.github.com>
    
    Co-authored-by: Craig Morten <cmorten@users.noreply.github.com>

---
+++ plugins/string/mod.ts
@@ -0,0 +1,67 @@
+/**
+ * Derived from <https://github.com/TrySound/rollup-plugin-string>
+ * 
+ * Licensed as follows:
+ * 
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) Bogdan Chadkin <trysound@yandex.ru>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import type { FilterPattern, Plugin } from "../../mod.ts";
+
+import { createFilter } from "../../src/rollup/createFilter.ts";
+
+export interface RollupStringOptions {
+
+  /**
+   * Specify include files
+   */
+  include: FilterPattern;
+
+  /**
+   * Specify exclude files
+   */
+  exclude?: FilterPattern;
+}
+
+export function string(opts: RollupStringOptions = {}) : Plugin {
+
+  if (!opts.include) {
+    throw Error("include option should be specified");
+  }
+
+  const filter = createFilter(opts.include, opts.exclude);
+
+  return {
+
+    name: "string",
+
+    transform(code, id) {
+      if (filter(id)) {
+        return {
+          code: `export default ${JSON.stringify(code)};`,
+          map: { mappings: "" }
+        };
+      }
+    }
+  };
+}
